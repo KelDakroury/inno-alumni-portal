4b6c1b184e5af9e48d224ad63f170e4bd6b9f62d
k.eldakroury <k.eldakroury@innopolis.university>
2024-03-03 22:59:28 +0300

    Document hocs module

--- src/hocs/with-auth-guard.js
+++ src/hocs/with-auth-guard.js
@@ -1,7 +1,13 @@
-import { AuthGuard } from 'src/guards/auth-guard';
+import { AuthGuard } from 'src/guards/auth-guard'; // Importing AuthGuard component from auth-guard file
 
+/**
+ * HOC (Higher Order Component) to wrap a component with authentication guarding.
+ * It ensures that the wrapped component is only rendered if the user is authenticated.
+ * @param {React.ComponentType} Component - The component to be wrapped with authentication guarding.
+ * @returns {React.ComponentType} - The wrapped component.
+ */
 export const withAuthGuard = (Component) => (props) => (
   <AuthGuard>
-    <Component {...props} />
+    <Component {...props} /> {/* Rendering the wrapped component with its props */}
   </AuthGuard>
 );
