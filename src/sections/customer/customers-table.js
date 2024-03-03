a25ebef01efd4f75489f58f2ba8dd79300f764a9
k.eldakroury <k.eldakroury@innopolis.university>
2024-03-03 23:25:19 +0300

    Document customer section

--- src/sections/customer/customers-table.js
+++ src/sections/customer/customers-table.js
@@ -17,6 +17,22 @@ import {
 import { Scrollbar } from 'src/components/scrollbar';
 import { getInitials } from 'src/utils/get-initials';
 
+/**
+ * Component representing an information table.
+ * @param {Object} props - The props object containing various parameters.
+ * @param {number} [props.count=0] - The total count of items.
+ * @param {Array} [props.items=[]] - An array containing items to be displayed in the table.
+ * @param {Function} props.onDeselectAll - Function to handle deselecting all items.
+ * @param {Function} props.onDeselectOne - Function to handle deselecting a single item.
+ * @param {Function} [props.onPageChange=()=>{}] - Function to handle page change.
+ * @param {Function} props.onRowsPerPageChange - Function to handle change in rows per page.
+ * @param {Function} props.onSelectAll - Function to handle selecting all items.
+ * @param {Function} props.onSelectOne - Function to handle selecting a single item.
+ * @param {number} [props.page=0] - The current page number.
+ * @param {number} [props.rowsPerPage=0] - The number of rows per page.
+ * @param {Array} [props.selected=[]] - An array containing IDs of selected items.
+ * @returns {JSX.Element} - Returns JSX for rendering the information table.
+ */
 export const InformationTable = (props) => {
   const {
     count = 0,
@@ -141,6 +157,21 @@ export const InformationTable = (props) => {
   );
 };
 
+/**
+ * PropTypes for the InformationTable component.
+ * @type {Object}
+ * @property {number} [count] - The total count of items.
+ * @property {Array} [items] - An array containing items to be displayed in the table.
+ * @property {Function} onDeselectAll - Function to handle deselecting all items.
+ * @property {Function} onDeselectOne - Function to handle deselecting a single item.
+ * @property {Function} [onPageChange] - Function to handle page change.
+ * @property {Function} onRowsPerPageChange - Function to handle change in rows per page.
+ * @property {Function} onSelectAll - Function to handle selecting all items.
+ * @property {Function} onSelectOne - Function to handle selecting a single item.
+ * @property {number} [page] - The current page number.
+ * @property {number} [rowsPerPage] - The number of rows per page.
+ * @property {Array} [selected] - An array containing IDs of selected items.
+ */
 InformationTable.propTypes = {
   count: PropTypes.number,
   items: PropTypes.array,
